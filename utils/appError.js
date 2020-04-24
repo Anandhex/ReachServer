f4f00cccf5d71d2d9640cb863b35065d28cb0156
Anand Patil <anand@preceptlabs.com>
2020-04-24 21:10:23 +0530

    commit

--- utils/appError.js
+++ utils/appError.js
@@ -1,7 +1,7 @@
 class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
-
+    console.log(message);
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true; //Whether the error is created by the express applicationa
